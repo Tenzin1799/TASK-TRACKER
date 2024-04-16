20a55f9aa29e131edc9ec1af883d0c2b7981c539
Tenzin1799 <131330402+Tenzin1799@users.noreply.github.com>
2024-04-16 18:06:30 -0500

    Deleted SceneController, Day, and commented out database requirement in module-info.java

--- src/main/java/com/example/tasktracker/WeekView.java
+++ src/main/java/com/example/tasktracker/WeekView.java
@@ -5,7 +5,6 @@ import javafx.scene.layout.GridPane;
 
 public class WeekView {
     private final int SINGLE_DAY = 1;
-
     public void addEventToCalendar(GridPane week, Button button, int dayCol, int startTime, int duration){
        //.add(button, colIndex, rowIndex, colSpan, rowSpan)
         week.add(button, dayCol, startTime, SINGLE_DAY, duration);
